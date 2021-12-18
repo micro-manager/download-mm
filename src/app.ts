a04d5df92068c69075276f0ae0ad0eb8b3863745
Mark A. Tsuchida <matsuchida@wisc.edu>
2021-12-18 09:35:28 -0600

    Use helmet (security settings including HSTS)

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express = require('express');
+import helmet = require('helmet');
 
 const PORT = Number(process.env.PORT) || 8080;
 
 const app = express();
+
+app.use(helmet());
 
 app.get('/', (req, res) => {
   res.status(200).send('Hello, World!').end();
